2569f7156e5b497db0a734ee00e672d440c0c9ca
Carl Morris <carl.morris.world@outlook.com>
2020-02-07 07:54:19 -0800

    Files generated by PkgTemplates

---
+++ src/TSExplore.jl
@@ -0,0 +1,5 @@
+module TSExplore
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using TSExplore
+using Test
+
+@testset "TSExplore.jl" begin
+    # Write your own tests here.
+end
